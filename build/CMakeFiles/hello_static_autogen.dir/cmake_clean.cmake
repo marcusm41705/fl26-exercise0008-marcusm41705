eaa9f926494c26adfea8bccf4b94c08f6d11a942
Marcus Mason <marcusm41705@vt.edu>
2026-09-16 02:16:48 +0000

    Added label_printer

---
+++ build/CMakeFiles/hello_static_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_static_autogen"
+  "hello_static_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/hello_static_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
